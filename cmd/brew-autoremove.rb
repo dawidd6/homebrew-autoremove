ec65b95ee5014b40fa8f869c7dd9382cce99ab41
Dawid Dziurla <dawidd0811@gmail.com>
2019-09-17 13:15:14 +0200

    more homebrewish structure

--- cmd/brew-autoremove.rb
+++ cmd/brew-autoremove.rb
@@ -10,59 +10,62 @@
 require "json"
 require "optparse"
 
-def package_needed?(packages, package)
-  return true if package["installed"].first["installed_on_request"]
+module Homebrew
+  module_function
 
-  packages.each do |p|
-    p["dependencies"].each do |dep|
-      return true if dep == package["name"]
+  def package_needed?(packages, pkg)
+    return true if pkg["installed"].first["installed_on_request"]
+
+    packages.each do |package|
+      deps = package["installed"].first["runtime_dependencies"]
+      next if deps.nil?
+
+      deps.each do |dep|
+        return true if dep["full_name"] == pkg["full_name"]
+      end
     end
+
+    false
   end
 
-  false
-end
+  def get_removable_packages(packages)
+    removables = []
 
-def get_removable_packages(packages)
-  removables = []
+    packages.each do |package|
+      next if package_needed?(packages, package)
 
-  packages.each do |package|
-    next if package_needed?(packages, package)
+      packages = packages.delete_if { |p| p == package }
+      removables = removables.append(package)
+      removables = removables.concat(get_removable_packages(packages))
+      break
+    end
 
-    packages = packages.delete_if { |p| p == package }
-    removables = removables.append(package)
-    removables = removables.concat(get_removable_packages(packages))
-    break
+    removables
   end
 
-  removables
+  dry = false
+  force = false
+  parser = OptionParser.new
+  parser.on("-n", "--dry-run", "") { dry = true }
+  parser.on("-f", "--force", "") { force = true }
+  parser.parse!
+
+  json = JSON.parse(`brew info --json --installed`)
+  removables = get_removable_packages(json)
+  names = removables.map { |r| r["name"] }
+
+  exit if removables.length.zero?
+
+  oh1("Removable packages: " \
+    "#{names.map(&Formatter.method(:identifier)).to_sentence}",
+    truncate: false)
+
+  exit if dry
+
+  unless force
+    ohai("Confirm?")
+    readline
+  end
+
+  system("brew", "remove", *names)
 end
-
-def parse
-  options = {}
-  OptionParser.new do |opts|
-    opts.on("-n", "--dry-run", "") do
-      options[:dry] = true
-    end
-    opts.on("-f", "--force", "") do
-      options[:force] = true
-    end
-  end.parse!
-  options
-end
-
-options = parse
-json = JSON.parse(`brew info --json --installed`)
-removables = get_removable_packages(json)
-removables.each do |r|
-  puts r["name"]
-end
-
-exit if removables.length.zero?
-exit if options[:dry]
-
-unless options[:force]
-  puts "\n==> Confirm?"
-  readline
-end
-
-system "brew", "remove", *removables.map { |r| r["name"] }
