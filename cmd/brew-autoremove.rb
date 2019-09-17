832d6fc90027498db198cddcd3daaca1c6c5b228
Dawid Dziurla <dawidd0811@gmail.com>
2019-09-17 15:18:04 +0200

    improvements

--- cmd/brew-autoremove.rb
+++ cmd/brew-autoremove.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 #:  * `autoremove` [options]
 #:
 #:  Remove packages that are no longer needed.
@@ -11,27 +9,37 @@ require "json"
 require "optparse"
 
 module Homebrew
-  module_function
+  module_function # all below functions become static
 
-  def package_needed?(packages, pkg)
-    return true if pkg["installed"].first["installed_on_request"]
+  # check if package was installed on request
+  def package_requested?(package)
+    package["installed"].each do |i|
+      return true if i["installed_on_request"]
+    end
 
-    packages.each do |package|
-      deps = package["installed"].first["runtime_dependencies"]
-      next if deps.nil?
+    false
+  end
 
-      deps.each do |dep|
-        return true if dep["full_name"] == pkg["full_name"]
+  # check if package is needed by another
+  def package_needed?(packages, package)
+    packages.each do |p|
+      p["installed"].each do |i|
+        # need to be safe, cause 'runtime_dependencies' field might not exist
+        i["runtime_dependencies"]&.each do |d|
+          return true if d["full_name"] == package["full_name"]
+        end
       end
     end
 
     false
   end
 
+  # get packages that could be safely removed
   def get_removable_packages(packages)
     removables = []
 
     packages.each do |package|
+      next if package_requested?(package)
       next if package_needed?(packages, package)
 
       packages = packages.delete_if { |p| p == package }
@@ -52,7 +60,7 @@ module Homebrew
 
   json = JSON.parse(`brew info --json --installed`)
   removables = get_removable_packages(json)
-  names = removables.map { |r| r["name"] }
+  names = removables.map { |r| r["full_name"] }
 
   exit if removables.length.zero?
 
@@ -63,7 +71,14 @@ module Homebrew
   exit if dry
 
   unless force
-    ohai("Confirm?")
+    ohai("Proceed?", <<~EOF
+      Enter:  yes
+      CTRL-C: no
+
+      To mark package as not removable run:
+          brew install PACKAGE
+    EOF
+    )
     readline
   end
 
